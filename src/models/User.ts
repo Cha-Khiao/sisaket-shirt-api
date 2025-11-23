84790cf3ed515cd92aa676ade7aaf0320b8c3dd1
Cha-Khiao <p.prasopphol203@gmail.com>
2025-11-23 12:23:32 +0700

    refactor models-user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,15 @@
-// src/models/User.ts
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
-  username?: string; // สำหรับ Admin
-  phone?: string;    // สำหรับ User ทั่วไป
-  password: string;  // เก็บแบบ Hash (เข้ารหัสแล้ว)
+  username?: string;
+  phone?: string;
+  password: string;
   name: string;
   role: 'admin' | 'user';
 }
 
 const UserSchema: Schema = new Schema({
-  username: { type: String, unique: true, sparse: true }, // sparse: true เพื่อให้ค่า null ไม่ซ้ำกันได้
+  username: { type: String, unique: true, sparse: true },
   phone: { type: String, unique: true, sparse: true },
   password: { type: String, required: true },
   name: { type: String, default: 'Member' },
